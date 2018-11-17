e290e364693c3afc9a3e9b72d77385ac48562404
Haoran Feng <fenghaoran@qutoutiao.net>
2018-11-17 03:04:02 +0000

    go on

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 import AppRouter from './AppRouter'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
-
+// ReactDOM.render(<AppRouter />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
